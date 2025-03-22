9afa14fb1a8fb27af77385c5a834ed0a7cdb61ae
Сергей Деревянко <serui8558@example.com>
2025-03-22 19:31:02 +0300

    Создан конструктор класса Ball

--- Ping-PongClassLibrary/Ball.cs
+++ Ping-PongClassLibrary/Ball.cs
@@ -8,6 +8,19 @@ namespace Ping_PongClassLibrary
 {
     public class Ball
     {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Vx { get; private set; }
+        public double Vy { get; private set; }
+        public double Radius { get; private set; }
+        public double Mass { get; private set; }
 
+        public Ball(double x, double y,double radius, double mass)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+            Mass = mass;
+        }
     }
 }
