ff99e60bdfc542a1019db86d43f990f1adc173c8
Сергей Деревянко <serui8558@example.com>
2025-04-12 20:42:50 +0300

    Интерфейс IBonus

---
+++ Ping-PongClassLibrary/IBonus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ping_PongClassLibrary
+{
+    public interface IBonus
+    {
+        double X {  get; set; }
+        double Y { get; set; }
+        double Width { get;}
+        double Height { get;}
+    }
+}
