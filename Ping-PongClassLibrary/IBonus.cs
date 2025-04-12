1e62cd85e960b91634a5771a0233520c053e38a4
Сергей Деревянко <serui8558@example.com>
2025-04-12 20:51:52 +0300

    Добавлено перечисление BonusType

--- Ping-PongClassLibrary/IBonus.cs
+++ Ping-PongClassLibrary/IBonus.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Ping_PongClassLibrary
 {
+    public enum BonusType
+    {
+        Size,
+        Length,
+        Material
+    }
     public interface IBonus
     {
+        BonusType Type { get; }
         double X {  get; set; }
         double Y { get; set; }
         double Width { get;}
