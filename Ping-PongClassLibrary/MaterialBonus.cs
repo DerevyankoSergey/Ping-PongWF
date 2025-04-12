27b28796dab4bd4196482217f4be23d9ec4580cd
Сергей Деревянко <serui8558@example.com>
2025-04-12 20:59:39 +0300

    Класс MaterialBonus реализующий интерфейс IBonus

---
+++ Ping-PongClassLibrary/MaterialBonus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ping_PongClassLibrary
+{
+    public class MaterialBonus : IBonus
+    {
+        public BonusType Type => BonusType.Material;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width => 20;
+        public double Height => 20;
+    }
+}
